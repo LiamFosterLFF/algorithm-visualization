b28d0adc3e0b98abcb9dffc060a50a7fb9dfff18
lf639 <lf639@bath.ac.uk>
2020-12-20 15:30:54 +0000

    Fixed navbar so it looks a little nicer using bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from 'react-bootstrap/Nav';
 import './app.css';
 
 import Sorting from './Sorting';
@@ -10,14 +11,31 @@ const App = () => {
     return (
         <div className="page">
              <Router>
-                <nav className="nav-bar">
+             <Nav activeKey="/main" onSelect={(selectedKey) => alert(`selected ${selectedKey}`)} >
+                <Nav.Item>
+                    <Nav.Link>
+                        <Link to="/">Main</Link>
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link>
+                        <Link to="/sorting">Sorting</Link>
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link>
+                        <Link to="/pathfinding">Pathfinding</Link>
+                    </Nav.Link>
+                </Nav.Item>
+            </Nav>
+                {/* <nav className="nav-bar">
                     <div>
                         <ul className="chart-types">
                             <li><Link className="chart-link" id="sorting"to="/sorting">Sorting</Link></li>
                             <li><Link  className="chart-link" id="pathfinding" to="/pathfinding">Pathfinding</Link></li>
                         </ul>
                     </div>
-                </nav>
+                </nav> */}
 
                 <Switch>
                     <Route path="/sorting">
@@ -33,3 +51,5 @@ const App = () => {
 };
 
 export default App;
+
+// Buttons ugly, use bootstrap
