fe22f4bf5fbb3573b986f8d620d26152b38bbe08
lf639 <lf639@bath.ac.uk>
2021-05-26 15:55:42 +0100

    Pathfinding: Added label to animation speed slider

--- src/Slider.js
+++ src/Slider.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Slider = (props) => {
     return (
         <div className="sliders-bar">
+            <label for="myRange">Animation Speed</label>
             <input onChange={(e) => props.setValue(e.target.value)} type="range" min="1" max="100" value={props.value} class="slider" id="myRange"></input>
         </div>
     )
